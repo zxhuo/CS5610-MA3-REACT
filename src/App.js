d4b2d10002f7f4882526c0c9cd1f3462e62206cf
Oliver Huo <oliverhuo@Olivers-MacBook-Pro.local>
2022-03-09 22:34:28 -0800

    update

--- src/App.js
+++ src/App.js
@@ -3,21 +3,24 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <div class="header">
+          <h1>Zhixiang's Website</h1>
+      </div>
+      <div class="left-sidebar">
+          <div class="navbar">
+              <ul>
+                  <li>Home Page</li>
+                  <li>Photos</li>
+                  <li>Secret Project</li>
+              </ul>
+          </div>
+      </div>
+      <div class="main">
+          <div class="box"></div>
+      </div>
+      <div class="right-sidebar"></div>
+      <div class="footer"></div>
     </div>
   );
 }
